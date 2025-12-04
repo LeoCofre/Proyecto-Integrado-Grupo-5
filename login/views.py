2860d309776a63e488ba1a5d4767dd3d9e393203
LeoCofre <leocofre1981@gmail.com>
2025-12-04 20:17:03 -0300

    Descarga dev2

--- login/views.py
+++ login/views.py
@@ -1,7 +1,6 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.hashers import make_password, check_password
-from django.contrib.auth.decorators import login_required
 from django.contrib import messages
 from .forms import CrearUsuarioForm, LoginForm # Asegúrate de importar tus forms
 from .models import Usuario
@@ -153,6 +152,12 @@ def login_view(request):
                         return redirect('panel_supervisor')
                     elif rol_actual == 'Administrador TI':
                         return redirect('crear_usuario')
+                    elif rol_actual == 'Auditor Interno':
+                        return redirect('panel_auditor')
+                    elif rol_actual == 'Enfermero':
+                        return redirect('panel_enfermero')
+                    elif rol_actual == 'SOME':
+                        return redirect('panel_some')
                     else:
                         return redirect('home')
                 else:
@@ -169,3 +174,10 @@ def logout_view(request):
     logout(request)
     messages.info(request, "Has cerrado sesión correctamente.")
     return redirect('login')
+
+def panel_matrona(request):
+    # Check for custom session-based authentication
+    if 'usuario_id' not in request.session:
+        messages.warning(request, "Debes iniciar sesión primero.")
+        return redirect('login')
+    return render(request, 'login/panel_matrona.html')
